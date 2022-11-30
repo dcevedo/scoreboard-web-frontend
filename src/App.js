06d0e215374184cc3613b5296c4cbe2e006658f7
dcevedo <37398700+dcevedo@users.noreply.github.com>
2022-11-30 13:52:28 -0500

    Basic CRUD local

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import CrudApp from "./components/CrudApp";
+// import './App.scss';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "./index.css"
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="text-center">Ejercicios con react</h1>
+      <CrudApp/>
     </div>
   );
 }
---
+++ src/components/CrudApp.js
@@ -0,0 +1,71 @@
+import React,{useState} from 'react';
+import CrudForm from './CrudForm';
+import CrudTable from './CrudTable';
+
+const initialDb = [
+  {
+    id: 1,
+    name: "Seiya",
+    constellation: "Pegaso",
+  },
+  {
+    id: 2,
+    name: "Shiryu",
+    constellation: "Dragón",
+  },
+  {
+    id: 3,
+    name: "Hyoga",
+    constellation: "Cisne",
+  },
+  {
+    id: 4,
+    name: "Shun",
+    constellation: "Andrómeda",
+  },
+  {
+    id: 5,
+    name: "Ikki",
+    constellation: "Fénix",
+  },
+];
+
+const CrudApp = () => {
+  const [db, setDb] = useState(initialDb);
+  const [dataToEdit, setDataToEdit] = useState(null);
+  
+  const createData = (data) => {
+    data.id = Date.now();
+    setDb([...db, data]);
+  }
+  const updateData = (data) => {
+    let newData = db.map((el) => el.id === data.id ? data: el );
+    setDb(newData);
+  }
+
+  const deleteData = (id) => {
+    let isConfirm = window.confirm(`¿Estas seguro de eliminar el registro con el id ${id}`);
+    if(isConfirm){
+      let newData = db.filter(el => el.id !== id);
+      setDb(newData);
+    }
+  }
+
+  return (
+    <div>
+      <h2>CRUD APP</h2>
+      <CrudForm 
+        createData={createData} 
+        updateData={updateData} 
+        dataToEdit={dataToEdit} 
+        setDataToEdit={setDataToEdit}/>
+      <hr></hr>
+      <CrudTable 
+        data={db} 
+        setDataToEdit={setDataToEdit} 
+        deleteData={deleteData}/>
+    </div>
+  )
+}
+
+export default CrudApp
