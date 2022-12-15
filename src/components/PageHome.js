7ce400788e031e2b574e80d0575883d96618e7b0
dcevedo <37398700+dcevedo@users.noreply.github.com>
2022-12-15 15:30:53 -0500

    Add a basic home page

---
+++ src/components/PageHome.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PageHome = () => {
+  return (
+    <div className="col-md-10 offset-md-1">
+      <h2 className="text-center">Bienvenido a tu web de marcadores de deportes!</h2>
+      <p>Aqui puedes ver los marcardores recientes de los partidos de tus deportes favoritos.</p>
+    </div>
+  );
+};
+
+export default PageHome;
